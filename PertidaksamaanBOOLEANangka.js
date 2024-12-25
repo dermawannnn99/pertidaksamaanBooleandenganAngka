71a25eca66b6b90dae061a3bb730eaf43a6e069d
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-25 13:47:41 +0700

    Add files via upload

---
+++ PertidaksamaanBOOLEANangka.js
@@ -0,0 +1,17 @@
+/**
+ * Perbandingan pertidaksamaan dengan boolean
+ * dengan angka
+ */
+
+let BoolPerbandinganPertama = ( 2 < 8 ) //true, karena benar 2 lebih kecil daripada 8
+console.log (BoolPerbandinganPertama)
+
+let BoolPerbandinganKedua = ( 9 < 3 ) //false, karena salah 9 lebih besar dari 3, yang benar 9 lebih kecil dari 3
+console.log (BoolPerbandinganKedua)
+
+let BoolPerbandinganKetiga = ( 2 == 2)  // trure, karena benar 2 sama dengan 2
+console.log (BoolPerbandinganKetiga)
+
+let bool1 = (2 > 3) //false, karena yang benar 2 lebih kecil daripada 3
+console.log(bool1)
+
